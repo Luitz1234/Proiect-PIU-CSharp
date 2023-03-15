fa9937f55aeb8c2dbcd7a0806ab63ecbd3b8b55b
Luitz1234 <101130435+Luitz1234@users.noreply.github.com>
2023-03-15 17:49:58 +0200

    Creare Clase

---
+++ Furnizor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librarii
+{
+    public class Furnizor
+    {
+        public string nume_furnizori;
+        public string adresa_furnizori;
+        public string numar_telefon;
+    }
+}
---
+++ Produs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librarii
+{
+    internal class Produs
+    {
+        private int idProdus;
+        public string nume_produs;
+        public string tip;
+        public string data_expirare;
+    }
+}
